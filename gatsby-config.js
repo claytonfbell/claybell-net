f5e72a6bf05f82a739fead4557748a42f5f9e9fd
Clay Bell <clay.bell@etix.com>
2019-08-12 18:53:22 -0700

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/baseline-computer-24px.png`, // This path is relative to the root of the site.
+        icon: `src/images/clay.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-material-ui`,
