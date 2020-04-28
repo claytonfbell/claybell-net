1dd6cf13d0b7af98c3938cda6c04fd7aa944c3f5
Clay Bell <clay.bell@etix.com>
2020-04-27 17:11:54 -0700

    reinstate default layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import React from "react"
-import LayoutBase from "../components/LayoutBase"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <LayoutBase>
+  <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </LayoutBase>
+  </Layout>
 )
 
 export default NotFoundPage
