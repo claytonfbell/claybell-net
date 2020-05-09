473b9d1da54725668054d4ec0321c8d6d0dd8d48
Clay Bell <clay.bell@etix.com>
2020-05-09 14:10:19 -0700

    Optimizing home page

---
+++ src/pages/home/PageCards.tsx
@@ -0,0 +1,134 @@
+import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+import Container from "@material-ui/core/Container"
+import Fade from "@material-ui/core/Fade"
+import Grid from "@material-ui/core/Grid"
+import makeStyles from "@material-ui/core/styles/makeStyles"
+import Typography from "@material-ui/core/Typography"
+import { graphql, Link, StaticQuery } from "gatsby"
+import React from "react"
+import useTimeout from "react-use/lib/useTimeout"
+import Footer from "../../components/Footer"
+import { useShowPrivate } from "../../components/ShowPrivateProvider"
+import { pages } from "../../pages"
+
+const useStyles = makeStyles(theme => ({
+  cardGrid: {
+    paddingTop: theme.spacing(6),
+    paddingBottom: theme.spacing(6),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+    borderBottom: "1px solid " + theme.palette.divider,
+    "&:hover": {
+      opacity: 0.75,
+    },
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(2),
+  },
+  cardLink: {
+    color: theme.palette.text.primary,
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+}))
+
+interface Data {
+  allImageSharp: {
+    edges: [
+      {
+        node: {
+          resize: {
+            src: string
+          }
+          fluid: {
+            originalName: string
+            src: string
+          }
+        }
+      }
+    ]
+  }
+}
+
+export default function PageCards() {
+  const classes = useStyles()
+  const showSecond = useTimeout(250)
+  const { showPrivate } = useShowPrivate()
+
+  return (
+    <StaticQuery
+      query={graphql`
+        {
+          allImageSharp(sort: { fields: [fluid___originalName], order: ASC }) {
+            edges {
+              node {
+                resize(width: 300, height: 200) {
+                  src
+                }
+                fluid {
+                  originalName
+                  src
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data: Data) => (
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Fade in={showSecond}>
+            <Grid container spacing={2} justify="center">
+              {pages
+                .filter(x => x.displayOnHome)
+                .filter(x => !x.isPrivate || showPrivate === "yes")
+                .map(page => (
+                  <Grid item xs={10} sm={6} md={4} lg={3} key={page.route}>
+                    <Card className={classes.card}>
+                      <Link to={page.route}>
+                        <CardMedia
+                          className={classes.cardMedia}
+                          title={page.title}
+                          image={
+                            data.allImageSharp.edges.find(
+                              x =>
+                                page.image.indexOf(
+                                  x.node.fluid.originalName
+                                ) !== -1
+                            ).node.resize.src
+                          }
+                        />
+                      </Link>
+                      <CardContent className={classes.cardContent}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          <Link className={classes.cardLink} to={page.route}>
+                            {page.title}
+                          </Link>
+                        </Typography>
+
+                        <Typography>{page.description}</Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+            </Grid>
+          </Fade>
+          <Footer />
+        </Container>
+      )}
+    />
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,28 @@
 import Box from "@material-ui/core/Box"
-import Card from "@material-ui/core/Card"
-import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
 import Container from "@material-ui/core/Container"
 import Fade from "@material-ui/core/Fade"
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper"
 import makeStyles from "@material-ui/core/styles/makeStyles"
 import Typography from "@material-ui/core/Typography"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import { useDarkMode } from "material-ui-pack/dist/DarkModeProvider"
 import React from "react"
 import Gravatar from "react-gravatar"
 import useTimeout from "react-use/lib/useTimeout"
 import Contact from "../components/ContactDrawer"
-import Footer from "../components/Footer"
 import HomeTemplate from "../components/HomeTemplate"
 import SEOComponent from "../components/SEOComponent"
-import { useShowPrivate } from "../components/ShowPrivateProvider"
-import mobileDark from "../images/claybell-net/mobile-dark.png"
 import logoOnDark from "../images/logo-on-dark.svg"
 import logo from "../images/logo.svg"
-import { pages } from "../pages"
+import PageCards from "./home/PageCards"
 
 export const query = graphql`
-  {
-    allImageSharp(sort: { fields: [fluid___originalName], order: ASC }) {
-      edges {
-        node {
-          resize(width: 300, height: 200) {
-            src
-          }
-          fluid {
-            originalName
-            src
-          }
+  query {
+    mobileDark: file(relativePath: { eq: "claybell-net/mobile-dark.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
@@ -42,54 +30,27 @@ export const query = graphql`
 `
 
 const useStyles = makeStyles(theme => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
   heroContent: {
-    // backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
   heroButtons: {
     marginTop: theme.spacing(2),
   },
-  cardGrid: {
-    paddingTop: theme.spacing(6),
-    paddingBottom: theme.spacing(6),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-    borderBottom: "1px solid " + theme.palette.divider,
-    "&:hover": {
-      opacity: 0.75,
-    },
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(2),
-  },
-  dates: {
-    display: "block",
-    marginBottom: theme.spacing(1),
-  },
-  cardLink: {
-    color: theme.palette.text.primary,
-    textDecoration: "none",
-    "&:hover": {
-      textDecoration: "underline",
-    },
-  },
 }))
 
 interface Props {
   data: {
+    mobileDark: {
+      childImageSharp: {
+        fluid: {
+          base64: string
+          aspectRatio: number
+          src: string
+          srcSet: string
+          sizes: string
+        }
+      }
+    }
     allImageSharp: {
       edges: [
         {
@@ -111,13 +72,14 @@ interface Props {
 const IndexPageContent = (props: Props) => {
   const classes = useStyles()
   const showFirst = useTimeout(50)
-  const showSecond = useTimeout(250)
-  const { showPrivate } = useShowPrivate()
   const { darkMode } = useDarkMode()
 
   return (
     <>
-      <SEOComponent title="Clayton Bell" imageSrc={mobileDark} />
+      <SEOComponent
+        title="Clayton Bell"
+        imageSrc={props.data.mobileDark.childImageSharp.fluid.src}
+      />
       <Paper className={classes.heroContent}>
         <Fade in={true}>
           <Container maxWidth="sm">
@@ -146,44 +108,7 @@ const IndexPageContent = (props: Props) => {
           </Container>
         </Fade>
       </Paper>
-      <Container className={classes.cardGrid} maxWidth="md">
-        <Fade in={showSecond}>
-          <Grid container spacing={2} justify="center">
-            {pages
-              .filter(x => x.displayOnHome)
-              .filter(x => !x.isPrivate || showPrivate === "yes")
-              .map(page => (
-                <Grid item xs={10} sm={6} md={4} lg={3} key={page.route}>
-                  <Card className={classes.card}>
-                    <Link to={page.route}>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        title={page.title}
-                        image={
-                          props.data.allImageSharp.edges.find(
-                            x =>
-                              page.image.indexOf(x.node.fluid.originalName) !==
-                              -1
-                          ).node.resize.src
-                        }
-                      />
-                    </Link>
-                    <CardContent className={classes.cardContent}>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        <Link className={classes.cardLink} to={page.route}>
-                          {page.title}
-                        </Link>
-                      </Typography>
-
-                      <Typography>{page.description}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))}
-          </Grid>
-        </Fade>
-        <Footer />
-      </Container>
+      <PageCards />
     </>
   )
 }
