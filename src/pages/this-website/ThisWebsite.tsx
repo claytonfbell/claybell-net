476484a4ad65654fa5e466c01abcca97330ff884
Clay Bell <clay.bell@etix.com>
2020-05-09 13:06:40 -0700

    Optimized the this-website page with graphql image queries, added lighthouse score screenshot

--- src/pages/this-website/ThisWebsite.tsx
+++ src/pages/this-website/ThisWebsite.tsx
@@ -1,18 +1,67 @@
 import Grid from "@material-ui/core/Grid"
+import { graphql, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
 import { useDarkMode } from "material-ui-pack/dist/DarkModeProvider"
 import React from "react"
-import mobileDark from "../../images/claybell-net/mobile-dark.png"
-import mobile from "../../images/claybell-net/mobile.png"
-import ClickableImage from "../scan-tickets/ClickableImage"
 
-export default function ThisWebsite() {
+export default function ThisWebsite(props) {
   const { darkMode } = useDarkMode()
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={6} sm={4}>
-        <ClickableImage src={darkMode ? mobileDark : mobile} alt="TutuTix" />
-      </Grid>
-    </Grid>
+    <StaticQuery
+      query={graphql`
+        query {
+          mobile: file(relativePath: { eq: "claybell-net/mobile.png" }) {
+            childImageSharp {
+              fluid(maxWidth: 400) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          mobileDark: file(
+            relativePath: { eq: "claybell-net/mobile-dark.png" }
+          ) {
+            childImageSharp {
+              fluid(maxWidth: 400) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          lighthouse: file(
+            relativePath: { eq: "claybell-net/lighthouse.png" }
+          ) {
+            childImageSharp {
+              fluid(maxHeight: 400) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={9} md={6}>
+              <Img
+                fluid={data.lighthouse.childImageSharp.fluid}
+                alt="Mobile screenshot of this website"
+              />
+            </Grid>
+          </Grid>
+          <Grid container spacing={3}>
+            <Grid item xs={6} sm={4}>
+              <Img
+                fluid={
+                  darkMode
+                    ? data.mobileDark.childImageSharp.fluid
+                    : data.mobile.childImageSharp.fluid
+                }
+                alt="Mobile screenshot of this website"
+              />
+            </Grid>
+          </Grid>
+        </>
+      )}
+    />
   )
 }
