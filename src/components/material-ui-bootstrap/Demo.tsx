fe0c57bb1421a3f4bf4095153722fbb5bf5fb498
Clay Bell <clay.bell@etix.com>
2021-04-24 21:38:03 -0700

    Updated material-ui-bootstrap

--- src/components/material-ui-bootstrap/Demo.tsx
+++ src/components/material-ui-bootstrap/Demo.tsx
@@ -1,4 +1,5 @@
 import { Grid } from "@material-ui/core"
+import StarIcon from "@material-ui/icons/Star"
 import {
   Alert,
   bootstrapColors,
@@ -6,6 +7,10 @@ import {
   Card,
   CardContent,
   CardHeader,
+  Label,
+  Panel,
+  PanelBody,
+  PanelHeader,
 } from "material-ui-bootstrap"
 import { DarkModeToggle } from "material-ui-pack"
 import React from "react"
@@ -13,22 +18,39 @@ import React from "react"
 export default function Demo() {
   return (
     <Grid container spacing={2}>
+      <Grid item xs={12}>
+        {bootstrapColors.map(color => (
+          <span key={color} style={{ marginRight: 4, marginBottom: 4 }}>
+            <Button variant="contained" color={color} startIcon={<StarIcon />}>
+              {color}
+            </Button>
+          </span>
+        ))}
+      </Grid>
+      <Grid item xs={12}>
+        {bootstrapColors.map(color => (
+          <span key={color} style={{ marginRight: 4, marginBottom: 4 }}>
+            <Label color={color}>{color} label</Label>
+          </span>
+        ))}
+      </Grid>
       {bootstrapColors.map(color => (
-        <Grid item xs={6} md={4} lg={3}>
-          <Button fullWidth variant="contained" color={color}>
-            {color}
-          </Button>
-        </Grid>
-      ))}
-      {bootstrapColors.map(color => (
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} key={color}>
           <Alert color={color} dismissible>
             A bootstrap alert
           </Alert>
         </Grid>
       ))}
       {bootstrapColors.map(color => (
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} key={color}>
+          <Panel color={color}>
+            <PanelHeader>Panel</PanelHeader>
+            <PanelBody>A bootstrap 3 style panel</PanelBody>
+          </Panel>
+        </Grid>
+      ))}
+      {bootstrapColors.map(color => (
+        <Grid item xs={12} md={6} key={color}>
           <Card color={color}>
             <CardHeader>Cards</CardHeader>
             <CardContent>A bootstrap card</CardContent>
